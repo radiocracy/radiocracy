c847b5803805d5c7873983fe54576c1b63583253
Mike Hickman <mhickman84@gmail.com>
2011-04-17 17:07:58 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radiocracy::Application.config.secret_token = 'f733e3277464cdf23e28d00f5a43315c57dda045943ca9896c97be9149494096ed6f0a829b470cc600b3e5fad7994f57777b2cc3392a2854eaba10008415a8f2'
